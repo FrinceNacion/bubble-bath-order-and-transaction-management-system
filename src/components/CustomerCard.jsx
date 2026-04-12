7248d50d4cafde6f739e4fcc36a71a859ff210b8
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-04-12 12:53:54 +0800

    Update: replaced class to className

--- src/components/CustomerCard.jsx
+++ src/components/CustomerCard.jsx
@@ -6,17 +6,17 @@ function CustomerCard({ customer }) {
                 <div className="d-flex flex-column">
                     <h5 className="fw-semibold">{customer.name}</h5>
                     <div className="d-flex flex-column">
-                        <p className="text-muted m-1"><i class="bi bi-envelope-at me-2"></i> {customer.email}</p>
-                        <p className="text-muted m-1"><i class="bi bi-phone me-2"></i> {customer.phone}</p>
-                        <p className="text-muted m-1"><i class="bi bi-geo-alt me-2"></i> {customer.address}</p>
+                        <p className="text-muted m-1"><i className="bi bi-envelope-at me-2"></i> {customer.email}</p>
+                        <p className="text-muted m-1"><i className="bi bi-phone me-2"></i> {customer.phone}</p>
+                        <p className="text-muted m-1"><i className="bi bi-geo-alt me-2"></i> {customer.address}</p>
                     </div>
                 </div>
                 <div className="d-flex align-items-start gap-2">
                     <button className="btn btn-outline-secondary d-flex align-items-center">
-                        <i class="bi bi-pencil-square"></i>
+                        <i className="bi bi-pencil-square"></i>
                     </button>
                     <button className="btn btn-danger d-flex align-items-center">
-                        <i class="bi bi-trash"></i>
+                        <i className="bi bi-trash"></i>
                     </button>
                 </div>
             </div>
