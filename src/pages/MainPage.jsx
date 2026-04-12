a37bf7586e17061de7b84bf35968924abed5c306
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-04-12 13:06:34 +0800

    Add: MainPage for parent rendering with sidebar and other pages for children

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,31 @@
+import { Outlet, useNavigate } from "react-router-dom";
+import SideBar from "../components/SideBar";
+import Authenticate from "../utils/Authenticate";
+import { useEffect, useState } from "react";
+
+function MainPage() {
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const result = await Authenticate();
+            if(result.success){
+                setUser(result.user);
+            }else{
+                navigate('/login');
+            }
+        };
+
+        fetchUser();
+    },[]);
+  
+    return (
+        <div className="p-0 m-0 d-flex w-100 min-vh-100 bg-light">
+            <SideBar user={user}/>
+            <Outlet />
+        </div>
+    );
+}
+
+export default MainPage;
