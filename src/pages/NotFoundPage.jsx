3693247ac2addedda2499d0c94c30e36a2a323d6
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-04-13 15:55:14 +0800

    Update: UI update and home button

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,9 +1,14 @@
-
+import { Link } from "react-router-dom";
 function NotFoundPage() {
   return (
-    <div>
-      <h1>404 - Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+    <div className="container-fluid min-vh-100 d-flex align-items-center justify-content-center bg-light">
+      <div className="card border-0 d-flex p-5 shadow">
+        <div className="card-body p-4 text-center">
+          <h1 className="display-1">404</h1>
+          <p className="text-dark">Page Not Found</p>
+          <Link to="/dashboard" className="btn btn-primary mt-3">Go to Home</Link>
+        </div>
+      </div>
     </div>
   );
 }
