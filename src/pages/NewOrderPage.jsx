d4e3c229e9054ee80f7de4b33e7ab1bb0a691beb
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-04-25 22:09:56 +0800

    Add: initial new order page

---
+++ src/pages/NewOrderPage.jsx
@@ -0,0 +1,17 @@
+
+function NewOrderPage() {
+
+    return (
+        <main className="container flex-fill p-4 p-xl-5">
+            <div className="d-flex flex-column">
+                <h4 className="fw-semibold text-dark mb-1">Create New Order</h4>
+                <p className="text-secondary small mb-4">Add garment details and create a new order.</p>
+            </div>
+            <div className="d-flex flex-column">
+                
+            </div>
+        </main>
+    )
+}
+
+export default NewOrderPage;
