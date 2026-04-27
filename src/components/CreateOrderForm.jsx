95f5cb60233661e06a00bef57def993f2af9d6d2
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-04-27 16:19:10 +0800

    Update: refactor CreateOrderForm to use CustomerSelect and DueDatePicker components

--- src/components/CreateOrderForm.jsx
+++ src/components/CreateOrderForm.jsx
@@ -1,24 +1,22 @@
-
+import DueDatePicker from "./DueDatePicker";
+import CustomerSelect from "./CustomerSelect";
 function CreateOrderForm() {
-
-
     return (
         <>
             <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">Order Details</h5>
-                    <form>
+                    <form className="d-flex flex-row justify-content-evenly">
                         <div className="mb-3">
                             <label htmlFor="customerName" className="form-label text-black">Customer</label>
-                            <select name="customer" id="customerName" className="form-control" value={name} onChange={(e) => setName(e.target.value)} required>
-                                <option value="">Select Customer</option>
-                                <option value="John Doe">John Doe</option>
-                                <option value="Jane Smith">Jane Smith</option>
-                            </select>
+                            <CustomerSelect />
                         </div>
+                        <DueDatePicker />
                     </form>
                 </div>
             </div>
         </>
     )
 }
+
+export default CreateOrderForm;
